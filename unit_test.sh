ff2230a68d26284a5a724b41714015635b7e59e0
pdli <jenkins@pauline.amd.com>
2019-04-17 15:49:03 +0800

    Update log files

--- unit_test.sh
+++ unit_test.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 echo "*****************************************************************"
-echo "Run test on Ubuntu 18.04.2, kernel: 4.18.0, GCC Vesion: gcc-7.4.0"
-docker run --rm -it --mount type=bind,src=/home/jenkins/linux,dst=/opt/src/linux yy305196456/autoconf_ubuntu18.04.2 \
-    | grep "$@"
+echo "Distro - Ubuntu 18.04.2"
+if [ -z "$1" ]
+then
+        echo "Empty: yes";
+        exit 1;
+else
+        sudo docker run --rm -it --mount type=bind,src=/home/jenkins/linux,dst=/opt/src/linux yy305196456/autoconf_ubuntu18.04.2 \
+                | grep "$@"
+fi
 
 echo -e "\n****************************************************************"
-echo -e "Run test on RedHat 7.6, kernel: 3.10.0, GCC Vesion: gcc-4.8.5-36 "
-docker run --rm -it --mount type=bind,src=/home/jenkins/linux,dst=/opt/src/linux yy305196456/autoconf_rhel7.6 \
-    | grep "$@"
+echo -e "Distro - RedHat 7.6"
+if [ -z "$1" ]
+then
+        echo "Empty: yes";
+        exit 1;
+else
+        sudo docker run --rm -it --mount type=bind,src=/home/jenkins/linux,dst=/opt/src/linux yy305196456/autoconf_rhel7.6 \
+                | grep "$@"
+fi
 
 echo -e "\n***************************************************************"
-echo -e "Run test on RedHat 8.0, kernel: 4.18.0, GCC Vesion: gcc-8.2      "
-docker run --rm -it --mount type=bind,src=/home/jenkins/linux,dst=/opt/src/linux yy305196456/autoconf_rhel8.0 \
-    | grep "$@"
+echo -e "Distro - RedHat 8.0"
+if [ -z "$1" ]
+then
+        echo "Empty: yes";
+        exit 1;
+else
+        sudo docker run --rm -it --mount type=bind,src=/home/jenkins/linux,dst=/opt/src/linux yy305196456/autoconf_rhel8.0 \
+                | grep "$@"
+fi
 
 # drop unused containers
-docker container prune --force
+sudo docker container prune --force 1>/dev/null
